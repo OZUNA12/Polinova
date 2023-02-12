cdc1a5c1f55e2f0b37eec33903d7a3bcf50e05c8
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2023-02-12 09:32:13 -0600

    mail para server

--- backend/src/services/sendMail.js
+++ backend/src/services/sendMail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 // Configurar el servidor SMTP
-/*const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
@@ -9,16 +9,16 @@ const nodemailer = require('nodemailer');
       user: 'cotiapp.dev@gmail.com',
       pass: 'rdlccaogysizwrvs'
     }
-});*/
+});
 
-var transporter = nodemailer.createTransport({
+/*var transporter = nodemailer.createTransport({
   host: "sandbox.smtp.mailtrap.io",
   port: 2525,
   auth: {
     user: "f3bb68b5782b23",
     pass: "f90acb4f439a85"
   }
-});
+});*/
 
 const sendMail = async(to, subject, html, dir, callback)=>{
     const message = {
